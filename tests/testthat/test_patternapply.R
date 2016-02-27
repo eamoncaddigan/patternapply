5771eddbf0a67af7fc93e7a6da3e64ab0b18308a
eamoncaddigan <eamon.caddigan@gmail.com>
2016-02-26 21:38:13 -0500

    Better expect()

--- tests/testthat/test_patternapply.R
+++ tests/testthat/test_patternapply.R
@@ -10,7 +10,7 @@ attributes(patternapplyResultVector) <- NULL
 
 test_that("Simple call returned correct results", {
   expect_equal(patternapplyResultVector, as.list(c(letters[1:9], 10:26)))
-  expect_equal(class(patternapplyResult), "replacement_list")
+  expect_is(patternapplyResult, "replacement_list")
   expect_equal(attr(patternapplyResult, "col_names"), "match_1")
   expect_equal(attr(patternapplyResult, "match_index"),
                c(rep(2, 9), rep(1, 26-9)))
